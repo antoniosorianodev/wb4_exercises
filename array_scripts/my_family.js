d9cf0cfebccdf36e2bb500e4ae523936b14cb395
Antonio <asoriano@appdev.yearup.org>
2024-05-08 15:45:46 -0500

    completed my_family.js

---
+++ array_scripts/my_family.js
@@ -0,0 +1,15 @@
+"use strict"
+
+// created an array (list) of family members names
+let myFamilyArray = ["Antonio", "Me", "Myself", "I"];
+
+// get the number of items on the list (in this specific case it should be 4)
+let numberOfFamilyMembers = myFamilyArray.length;
+
+// loop over my family array and print the names
+for (let i = 0; i < numberOfFamilyMembers; i++) {
+
+    // print out each family member's name
+    console.log(myFamilyArray[i]);
+
+}
